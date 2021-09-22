7d1907e65c4180a71292a9ff6e6f3e67ba491551
Wayne Wee <wwayne.wee@gmail.com>
2021-09-22 10:18:31 +0800

    removed tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx}", "./components/**/*.{js,ts,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
